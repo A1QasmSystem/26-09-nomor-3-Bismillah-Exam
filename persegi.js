df4342472aedcffc2108f502ef2fc54d5a234aab
AlQasmSystem <alqasmsystem@localhost.localdomain>
2020-09-26 14:00:18 +0000

    Bismillah, Nomor 3

---
+++ persegi.js
@@ -0,0 +1,11 @@
+var panjang = 8;
+var lebar = 4;
+var jumlahluas = panjang * lebar;
+
+function jumlahluas(p, l) {
+  p * l;
+}
+
+document.write("Panjang: ", panjang, "<br>");
+document.write("Lebar: ", lebar, "<br>", "<br>");
+document.write("Luas Persegi Panjang: ", jumlahluas, "<br>");
